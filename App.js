18070e1a7e2c4cf4612011d2ad3e333100fd9711
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-05 21:42:57 +0000

    stylesheet updates

--- App.js
+++ App.js
@@ -4,12 +4,18 @@ import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
       <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World!</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }
+});
 
 export default App;
