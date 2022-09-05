2a85245f8e0a474c6c940535693a07cdff86f88c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-05 21:38:33 +0000

    stylesheet added

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: 'darkslateblue', fontSize: 20}}>Hello World!</Text>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default App;
