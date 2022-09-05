9ea58e05d055fa33afb2d956bb79044f65b001b0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-05 21:49:26 +0000

    image added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World!</Text>
+      <Text style={styles.text}>Hello World!</Text>
+      <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} 
+      style={styles.img} />
     </View>
   );
 };
@@ -15,6 +17,14 @@ const styles = StyleSheet.create({
     flex: 1, 
     justifyContent: 'center', 
     alignItems: 'center'
+  },
+  text: {
+    color: 'darkslateblue', 
+    fontSize: 30
+  },
+  img: {
+    width: 100,
+    height: 100,
   }
 });
 
